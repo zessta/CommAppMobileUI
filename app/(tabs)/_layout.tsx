88dd946189d8e4797e647a8d840e6e93005aa5d9
shivaburugula <shivakumar.b@truera.com>
2025-03-04 15:14:35 +0530

    reset project

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,16 +5,11 @@ import { Platform } from 'react-native';
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
