242f8dd00f23d428b932c8c7cdc873c15b4d2e87
shivaburugula <shivakumar.b@truera.com>
2025-03-11 22:47:32 +0530

    preffiey

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,14 @@
-import { useRouter } from "expo-router";
-import { View, Text, TouchableOpacity } from "react-native";
-import LoginScreen from "./LoginScreen";
-import { SocketProvider } from "@/components/SocketContext";
+import { useRouter } from 'expo-router';
+import { View, Text, TouchableOpacity } from 'react-native';
+import LoginScreen from './LoginScreen';
+import { SocketProvider } from '@/components/SocketContext';
 import 'react-native-get-random-values'; // Required for UUID support
 import 'react-native-websocket'; // WebSocket polyfill for React Native
 export default function HomeScreen() {
   const router = useRouter();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <LoginScreen />
     </View>
   );
