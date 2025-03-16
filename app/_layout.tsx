cc88ec5b6ebea2bad32dc204052453e97fc6bca1
shivaburugula <shivakumar.b@truera.com>
2025-03-16 16:16:14 +0530

    in groups

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,18 +2,21 @@ import { UserProvider } from '@/components/UserContext';
 import { Stack } from 'expo-router';
 import 'react-native-get-random-values'; // Required for UUID support
 import 'react-native-websocket'; // WebSocket polyfill for React Native
+import { TranslatorProvider } from 'react-native-translator'; // here
 
 export default function RootLayout() {
   return (
-    <UserProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="LoginScreen" options={{ headerShown: false }} />
-        <Stack.Screen name="GroupStack/GroupDetailsScreen" options={{ headerShown: false }} />
-        <Stack.Screen name="ChatStack/chatScreen" />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </UserProvider>
+    <TranslatorProvider>
+      <UserProvider>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="LoginScreen" options={{ headerShown: false }} />
+          <Stack.Screen name="GroupStack/GroupDetailsScreen" options={{ headerShown: false }} />
+          <Stack.Screen name="ChatStack/chatScreen" />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </UserProvider>
+    </TranslatorProvider>
   );
 }
